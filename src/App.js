3519bbb92f1ebdf5ed893ca61e7290791bd08efe
haneefsyedanywhere <haneef.syed@anywhere.co>
2022-07-30 22:15:39 +0530

    added HMR support

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {hot} from 'react-hot-loader';
 class App extends React.Component {
   state = {
     count: 0
@@ -17,4 +17,4 @@ class App extends React.Component {
   }
 }
 
-export default App
+export default hot(module)(App);
