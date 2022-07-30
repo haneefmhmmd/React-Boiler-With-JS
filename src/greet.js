e39ffc6c611bddb47e63704a8670b771260c6c68
haneefsyedanywhere <haneef.syed@anywhere.co>
2022-07-30 18:43:45 +0530

    integrated react, created webpack config files for dev and prod

--- src/greet.js
+++
@@ -1,5 +0,0 @@
-const greet = (name)=>{
-    console.log(`Hi ${name}`);
-}
-
-export default greet;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-import greet from "./greet";
+import React from "react";
+import ReactDOM  from "react-dom";
+import App from "./App";
 
-greet("haneef");
+
+ReactDOM.render(<App />, document.getElementById("root"));
