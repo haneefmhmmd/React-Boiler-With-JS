0c44902b382babcb00396df054fb82825e121ac7
haneefsyedanywhere <haneef.syed@anywhere.co>
2022-07-30 18:55:29 +0530

    added support for proposed js features

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-import React from "react";
+import React from 'react'
 
-export default function App(){
-    return (<div>Hello World!!</div>);
+class App extends React.Component {
+  state = {
+    count: 0
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+        <h2>Count: {this.state.count}</h2>
+        <button onClick={() => this.setState(state => ({count: state.count + 1}))}>+</button>
+        <button onClick={() => this.setState(state => ({count: state.count - 1}))}>-</button>
+      </div>
+    )
+  }
 }
+
+export default App
