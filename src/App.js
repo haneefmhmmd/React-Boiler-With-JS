3eb30333aaf1a61497ac1b72088c6a6866008594
haneefsyedanywhere <haneef.syed@anywhere.co>
2022-07-31 14:10:31 +0530

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
 class App extends React.Component {
-  state = {
-    count: 0,
-  }
-
   render() {
     return (
       <div>
         <h1>Hello World!</h1>
-        <h2>Count: {this.state.count}</h2>
-        <img src="./logo.png" alt="" />
-        <button
-          onClick={() => this.setState((state) => ({ count: state.count + 1 }))}
-        >
-          +
-        </button>
-        <button
-          onClick={() => this.setState((state) => ({ count: state.count - 1 }))}
-        >
-          -
-        </button>
       </div>
     )
   }
